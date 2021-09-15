6838c1b7b30e721140d9cd54c80a5cf9d763e878
Kleuder Lima <kleuder.lima@gmail.com>
2021-09-15 07:47:12 -0300

    CategoryResource

---
+++ backend/src/main/java/com/kslima/catalog/resources/CategoryResources.java
@@ -0,0 +1,24 @@
+package com.kslima.catalog.resources;
+
+import com.kslima.catalog.entities.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResources {
+
+    @GetMapping()
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> list = new ArrayList<>();
+        list.add(new Category(1L, "Books"));
+        list.add(new Category(2L, "Eletronics"));
+
+        return ResponseEntity.ok().body(list);
+    }
+}

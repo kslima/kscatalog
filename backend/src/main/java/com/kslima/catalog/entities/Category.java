34cb3396ed190e41405d64ece973dd015cc99b5f
Kleuder Lima <kleuder.lima@gmail.com>
2021-10-01 19:21:26 -0300

    ORM mapping, database

--- backend/src/main/java/com/kslima/catalog/entities/Category.java
+++ backend/src/main/java/com/kslima/catalog/entities/Category.java
@@ -1,11 +1,11 @@
 package com.kslima.catalog.entities;
 
-import com.kslima.catalog.dto.CategoryDTO;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -22,6 +22,9 @@ public class Category implements Serializable {
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant updatedAt;
+
+    @ManyToMany(mappedBy = "categories")
+    private final Set<Product> products = new HashSet<>();
 
     public Category() {
 
@@ -54,6 +57,10 @@ public class Category implements Serializable {
 
     public Instant getUpdatedAt() {
         return updatedAt;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @PrePersist

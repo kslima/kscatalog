763e9a2efa1bae49feb9c8708a6de919bee9404e
Kleuder Lima <kleuders@kyros.com.br>
2021-11-23 17:24:16 -0300

    NavBar style

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,13 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import NavBar from 'components/NavBar';
 
 function App() {
   return (
-    <h1>Hello DevSuperior</h1>
+    <>
+      <NavBar />
+      <h1>Hello DSCatalog</h1>
+    </>
   );
 }
 
